f5f1bb4a764820454419cbbe52e4f886671c1e38
Decklin Foster <decklin@red-bean.com>
2014-05-11 17:53:41 -0400

    Remove Python-Twisted dependencies (manually, not properly installed for autoremove)

--- provision-root-cleanup.sh
+++ provision-root-cleanup.sh
@@ -7,6 +7,10 @@ set -v
 rm -rf /usr/local/src/ruby-*
 rm -rf /usr/local/src/chruby-*
 
+# Remove dependencies of Twisted Python
+apt-get -y remove --purge libjs-jquery python-apport python-crypto python-dbus python-dbus-dev python-gdbm python-gi python-httplib2 python-keyring python-launchpadlib python-lazr.restfulclient python-lazr.uri python-openssl python-pam python-pkg-resources python-problem-report python-pycurl python-secretstorage python-simplejson python-twisted-bin python-twisted-core python-twisted-names python-twisted-web python-wadllib python-zope.interface
+apt-get -y autoremove
+
 # Clean up APT cache and zero out disk to reduce image size
 apt-get clean
 dd if=/dev/zero of=/EMPTY bs=1M
